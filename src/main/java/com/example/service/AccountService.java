fde404f3cca79359deee80b61ca8507f2a60dfa7
lily <lilyebadi@gmail.com>
2025-06-14 17:29:20 +0000

    User Registertion Implemented

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,30 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account register(Account account) {
+        if(account.getUsername() == null || account.getUsername().trim().isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be blank/empty");
+        }
+
+        if(account.getPassword() == null || account.getPassword().length() < 4) {
+            throw new IllegalArgumentException("Username cannot be blank/empty");
+        }
+
+        if (accountRepository.findByUsername(account.getUsername()).isPresent()) {
+            return null;
+        }
+
+        return accountRepository.save(account);
+    }
 }
